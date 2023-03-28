46885682487583786b3c78d9912ec1123547ebe2
frios <frios@jst.gob.ar>
2023-03-28 01:21:31 -0300

    implement songs with txt files instead of srt

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ fetch('./songs/')
       const fileName = link.href.split('/').pop();
       if (fileName.endsWith('.mp3')) {
         const srtFileName = fileName.replace('.mp3', '.srt');
-        songUrls[decodeURIComponent(fileName)] = decodeURIComponent(srtFileName);
+        const txtFileName = fileName.replace('.mp3', '.txt');
+        songUrls[decodeURIComponent(fileName)] = { srt: decodeURIComponent(srtFileName), txt: decodeURIComponent(txtFileName) };
       }
     }
 
@@ -21,7 +22,6 @@ fetch('./songs/')
       const button = document.createElement('button');
       button.textContent = songUrl.replace('.mp3', '');
       button.onclick = () => switchSong(songUrl);
-      console.log("songurl: " + songUrl)
       songListDropdown.appendChild(button);
     }
 
@@ -44,31 +44,75 @@ function switchSong(songFilename) {
   // clear the previous interval
   clearInterval(intervalId);
 
-  fetch('songs/' + songUrls[songFilename])
-    .then(response => response.text(console.log(response.url)))
-    .then(data => {
-      const srt = data.split(/\n\s*\n/).filter(Boolean).map(item => {
-        const [id, time, ...textLines] = item.trim().split('\n');
-        const [start, end] = time.split(' --> ').map(convertSrtTimeToSeconds);
-        return { id: parseInt(id), start, end, text: textLines.join(' ') };
-      });
+  const srtFilename = songUrls[songFilename].srt;
+  const srtUrl = `songs/${srtFilename}`;
+  const txtUrl = `songs/${songFilename.replace('.mp3', '.txt')}`;
 
-      // assign the interval to the intervalId variable
-      intervalId = setInterval(() => {
-        const currentTime = audio.currentTime;
-        const currentSrt = srt.find(item => item.start <= currentTime && item.end >= currentTime);
-        if (currentSrt) {
-          const currentIndex = srt.findIndex(item => item.id === currentSrt.id);
-          lyrics.innerHTML = srt.map((item, index) => `<div class="lyric ${index === currentIndex ? 'active' : ''}">${item.text.replace(/<br>/g, '')}</div>`).join('');
+  let url;
+  let fileType;
+
+  fetch(srtUrl)
+    .then(response => {
+      if (response.status === 404) {
+        fileType = 'txt';
+        url = txtUrl;
+        return fetch(txtUrl);
+      } else {
+        fileType = 'srt';
+        url = srtUrl;
+        return response.text();
+      }
+    })
+    .then(data => {
+      if (fileType === 'srt') {
+        // Parse the SRT file
+        const srt = data.split(/\n\s*\n/).filter(Boolean).map(item => {
+          const [id, time, ...textLines] = item.trim().split('\n');
+          const [start, end] = time.split(' --> ').map(convertSrtTimeToSeconds);
+          return { id: parseInt(id), start, end, text: textLines.join(' ') };
+        });
+
+        // assign the interval to the intervalId variable
+        intervalId = setInterval(() => {
+          const currentTime = audio.currentTime;
+          const currentSrt = srt.find(item => item.start <= currentTime && item.end >= currentTime);
+          if (currentSrt) {
+            const currentIndex = srt.findIndex(item => item.id === currentSrt.id);
+            lyrics.innerHTML = srt.map((item, index) => `<div class="lyric ${index === currentIndex ? 'active' : ''}">${item.text.replace(/<br>/g, '')}</div>`).join('');
+            const lyricsContainer = document.getElementById('lyrics-container');
+            const currentLyric = lyrics.querySelector('.lyric.active');
+            const scrollY = currentLyric.offsetTop - lyricsContainer.offsetHeight / 2 + currentLyric.offsetHeight / 2;
+            const behavior = audio.paused ? 'instant' : 'smooth';
+            lyricsContainer.scrollTo({ top: scrollY, behavior });
+          }
+        }, 100);
+      } else if (fileType === 'txt') {
+        // Load the text file content into the template
+        return fetch(url).then(response => response.text());
+      }
+    })
+    .then(response => {
+      if (fileType === 'txt') {
+        // Render the text file content in the template
+        lyrics.innerHTML = response.split('\n').map(line => `<div class="lyric">${line.trim()}</div>`).join('');
+        
+        // synchronize the scrolling with audio time
+        intervalId = setInterval(() => {
+          const currentTime = audio.currentTime;
+          const totalDuration = audio.duration;
+          const progressPercentage = (currentTime / totalDuration) * 100;
           const lyricsContainer = document.getElementById('lyrics-container');
-          const currentLyric = lyrics.querySelector('.lyric.active');
-          const scrollY = currentLyric.offsetTop - lyricsContainer.offsetHeight / 2 + currentLyric.offsetHeight / 2;
+          const containerHeight = lyricsContainer.offsetHeight;
+          const lyricsHeight = lyrics.offsetHeight;
+          const scrollY = (progressPercentage / 100) * (lyricsHeight - containerHeight);
           const behavior = audio.paused ? 'instant' : 'smooth';
           lyricsContainer.scrollTo({ top: scrollY, behavior });
-        }
-      }, 100);
-    });
+        }, 100);
+      }
+    })
+    .catch(error => console.error(error));
 }
+
 function convertSrtTimeToSeconds(time) {
   const [hours, minutes, seconds] = time.split(':').map(parseFloat);
   return hours * 3600 + minutes * 60 + seconds;
@@ -88,7 +132,6 @@ if (audioWrapper) {
     }, 3000);
   });
 }
-
 function activateAudioOnMouseMove() {
   // Get the audio element
   const audio = document.getElementById('audio');
